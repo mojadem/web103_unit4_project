43ce66b5bf85b78f5e83ceaa5770bea3121fe228
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-10-17 14:37:58 -0400

    add cars controller and route

---
+++ server/controllers/cars.js
@@ -0,0 +1,73 @@
+import { pool } from "../config/database.js";
+
+const getCars = async (req, res) => {
+  try {
+    const results = await pool.query("SELECT * FROM cars ORDER BY id ASC");
+    res.status(200).json(results.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(409).json({ error: err.message });
+  }
+};
+
+const getCar = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    const results = await pool.query("SELECT * FROM cars WHERE id = $1", [id]);
+    res.status(200).json(results.rows[0]);
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+};
+
+const createCar = async (req, res) => {
+  try {
+    const { name, price, is_convertible, roof, wheel, exterior, interior } =
+      req.body;
+
+    const results = await pool.query(
+      "INSERT INTO cars (name, price, is_convertible, roof, wheel, exterior, interior) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [name, price, is_convertible, roof, wheel, exterior, interior],
+    );
+    res.status(201).json(results.rows[0]);
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+    console.error(err);
+  }
+};
+
+const updateCar = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const { name, price, is_convertible, roof, wheel, exterior, interior } =
+      req.body;
+
+    const results = await pool.query(
+      "UPDATE cars SET name = $1, price = $2, is_convertible = $3, roof = $4, wheel = $5, exterior = $6, interior = $7 WHERE id = $8",
+      [name, price, is_convertible, roof, wheel, exterior, interior, id],
+    );
+    res.status(200).json(results.rows[0]);
+  } catch (err) {
+    response.status(409).json({ error: err.message });
+  }
+};
+
+const deleteCar = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    const results = await pool.query("DELETE FROM cars WHERE id = $1", [id]);
+    res.status(200).json(results.rows[0]);
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+};
+
+export default {
+  createCar,
+  getCars,
+  getCar,
+  updateCar,
+  deleteCar,
+};
--- server/server.js
+++ server/server.js
@@ -1,36 +1,30 @@
-import express from 'express'
-import path from 'path'
-import favicon from 'serve-favicon'
-import dotenv from 'dotenv'
+import express from "express";
+import path from "path";
+import favicon from "serve-favicon";
 
 // import the router from your routes file
+import carsRouter from "./routes/cars.js";
 
+const PORT = process.env.PORT || 3000;
 
-dotenv.config()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+app.use(express.json());
 
-const app = express()
-
-app.use(express.json())
-
-if (process.env.NODE_ENV === 'development') {
-    app.use(favicon(path.resolve('../', 'client', 'public', 'lightning.png')))
-}
-else if (process.env.NODE_ENV === 'production') {
-    app.use(favicon(path.resolve('public', 'lightning.png')))
-    app.use(express.static('public'))
+if (process.env.NODE_ENV === "development") {
+  app.use(favicon(path.resolve("../", "client", "public", "lightning.png")));
+} else if (process.env.NODE_ENV === "production") {
+  app.use(favicon(path.resolve("public", "lightning.png")));
+  app.use(express.static("public"));
 }
 
 // specify the api path for the server to use
+app.use("/api/cars", carsRouter);
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.get('/*', (_, res) =>
-        res.sendFile(path.resolve('public', 'index.html'))
-    )
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", (_, res) => res.sendFile(path.resolve("public", "index.html")));
 }
 
 app.listen(PORT, () => {
-    console.log(`server listening on http://localhost:${PORT}`)
-})
+  console.log(`server listening on http://localhost:${PORT}`);
+});
